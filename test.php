7ded151df9f196248263c8a864f089e9bca7a7dc
Alexander Minza <alexander.minza@gmail.com>
2025-10-14 13:06:37 +0300

    VB_MIA_Test vb_mia_pay

--- test.php
+++ test.php
@@ -4,22 +4,45 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Victoriabank\VictoriabankMia\VictoriabankMiaClient;
 
-// define('DEBUG', getenv('DEBUG'));
-// define('VB_MIA_BASE_URI', getenv('VB_MIA_BASE_URI'));
-// define('VB_MIA_USERNAME', getenv('VB_MIA_USERNAME'));
-// define('VB_MIA_PASSWORD', getenv('VB_MIA_PASSWORD'));
-// define('VB_IBAN', getenv('VB_IBAN'));
-// define('VB_PUBLIC_KEY_PATH', getenv('VB_PUBLIC_KEY_PATH'));
-
 
 class VB_MIA_Test
 {
+    #region Properties
+    /**
+     * @var bool
+     */
     private $DEBUG;
+
+    /**
+     * @var string
+     */
     private $VB_MIA_BASE_URI;
+
+    /**
+     * @var string
+     */
     private $VB_MIA_USERNAME;
+
+    /**
+     * @var string
+     */
     private $VB_MIA_PASSWORD;
-    private $VB_IBAN;
+
+    /**
+     * @var string
+     */
     private $VB_PUBLIC_KEY_PATH;
+
+    /**
+     * @var string
+     */
+    private $VB_COMPANY_IBAN;
+
+    /**
+     * @var string
+     */
+    private $VB_COMPANY_NAME;
+    #endregion
 
     public function __construct()
     {
@@ -27,8 +50,8 @@ class VB_MIA_Test
         $this->VB_MIA_BASE_URI = getenv('VB_MIA_BASE_URI');
         $this->VB_MIA_USERNAME = getenv('VB_MIA_USERNAME');
         $this->VB_MIA_PASSWORD = getenv('VB_MIA_PASSWORD');
-        $this->VB_IBAN = getenv('VB_IBAN');
         $this->VB_PUBLIC_KEY_PATH = getenv('VB_PUBLIC_KEY_PATH');
+        $this->VB_COMPANY_NAME = getenv('VB_COMPANY_NAME');
     }
 
     private function vb_mia_init_client()
@@ -63,11 +86,62 @@ class VB_MIA_Test
         return $token;
     }
 
+    /**
+     * @param VictoriabankMiaClient $client
+     * @param string $authToken
+     * @param string $order_id
+     * @param string $order_name
+     * @param float  $total_amount
+     * @param string $currency
+     * @param int    $validity_minutes
+     */
+    private function vb_mia_pay($client, $authToken, $order_id, $order_name, $total_amount, $currency, $validity_minutes)
+    {
+        $qr_data = array(
+            'header' => array(
+                'qrType' => 'DYNM', # Type of QR code: DYNM - Dynamic QR, STAT - Static QR, HYBR - Hybrid QR
+                'amountType' => 'Fixed', # Specifies the type of amount: Fixed - Dynamic QR, Controlled - Static QR, Free - Hybrid QR
+                'pmtContext' => 'e' #Payment context: m - mobile payment, e - e-commerce payment, i - invoice payment, 0 - other
+            ),
+            'extension' => array(
+                'creditorAccount' => array(
+                    'iban' => $this->VB_COMPANY_IBAN
+                ),
+                'amount' => array(
+                    'sum' => $total_amount,
+                    'currency' => $currency
+                ),
+                'dba' => $this->VB_COMPANY_NAME,
+                'remittanceInfo4Payer' => $order_name,
+                'creditorRef' => $order_id,
+                'ttl' => array(
+                    'length' => $validity_minutes, #The duration for which the QR code is valid
+                    'units' => 'mm' #The unit of time for the TTL: ss - seconds, mm - minutes
+                )
+            )
+        );
+
+        $create_qr_response = $client->createPayeeQr($qr_data, $authToken);
+        return $create_qr_response;
+    }
+
     public function test()
     {
         $client = $this->vb_mia_init_client();
-        $token = $this->vb_mia_generate_token($client);
-        print($token);
+        $authToken = $this->vb_mia_generate_token($client);
+        print($authToken);
+
+        $order_id = '12345';
+        $order_name = "Order #$order_id";
+        $total_amount = 123.45;
+        $currency = 'MDL';
+        $vb_mia_pay_response = $this->vb_mia_pay($client, $authToken, $order_id, $order_name, $total_amount, $currency, 10);
+
+        $qr_extension_id = $vb_mia_pay_response['qrExtensionUUID'];
+        $qr_url = $vb_mia_pay_response['qrAsText'];
+        $qr_image = $vb_mia_pay_response['qrAsImage'];
+        print($qr_extension_id);
+        print($qr_url);
     }
 }
 
