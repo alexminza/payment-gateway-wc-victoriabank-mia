753e16b0e60eb827e84fffe3d7918ae5393d7da9
Alexander Minza <alexander.minza@gmail.com>
2025-10-22 23:17:50 +0300

    assets

---
+++ assets/js/blocks.js
@@ -0,0 +1,44 @@
+const vb_mia_settings = window.wc.wcSettings.getSetting('vb_mia_data', {});
+const vb_mia_title = window.wp.htmlEntities.decodeEntities(vb_mia_settings.title);
+
+const vb_mia_content = () => {
+    return window.wp.htmlEntities.decodeEntities(vb_mia_settings.description || '');
+};
+
+const vb_mia_label = () => {
+    let icon = vb_mia_settings.icon
+        ? window.wp.element.createElement(
+            'img',
+            {
+                alt: vb_mia_title,
+                title: vb_mia_title,
+                src: vb_mia_settings.icon,
+                style: { float: 'right', paddingRight: '1em' }
+            }
+        )
+        : null;
+
+    let label = window.wp.element.createElement(
+        'span',
+        icon ? { style: { width: '100%' } } : null,
+        vb_mia_title,
+        icon
+    );
+
+    return label;
+};
+
+const vb_mia_blockGateway = {
+    name: vb_mia_settings.id,
+    label: Object(window.wp.element.createElement)(vb_mia_label, null),
+    icons: [{id: 'mia', alt: vb_mia_settings.title, src: vb_mia_settings.icon}],
+    content: Object(window.wp.element.createElement)(vb_mia_content, null),
+    edit: Object(window.wp.element.createElement)(vb_mia_content, null),
+    canMakePayment: () => true,
+    ariaLabel: vb_mia_title,
+    supports: {
+        features: vb_mia_settings.supports,
+    },
+};
+
+window.wc.wcBlocksRegistry.registerPaymentMethod(vb_mia_blockGateway);
