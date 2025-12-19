55fc3edd1a0ea642da27cc420f5dba257a1edda4
Alexander Minza <alexander.minza@gmail.com>
2025-12-19 17:17:09 +0200

    v1.0.2

--- payment-gateway-wc-victoriabank-mia.php
+++ payment-gateway-wc-victoriabank-mia.php
@@ -4,7 +4,7 @@
  * Plugin Name: Payment Gateway for Victoriabank MIA for WooCommerce
  * Description: Accept MIA Instant Payments directly on your store with the Payment Gateway for Victoriabank MIA for WooCommerce.
  * Plugin URI: https://github.com/alexminza/payment-gateway-wc-victoriabank-mia
- * Version: 1.0.1
+ * Version: 1.0.2
  * Author: Alexander Minza
  * Author URI: https://profiles.wordpress.org/alexminza
  * Developer: Alexander Minza
@@ -49,7 +49,7 @@ function victoriabank_mia_init()
         const MOD_ID             = 'victoriabank_mia';
         const MOD_TITLE          = 'Victoriabank MIA';
         const MOD_PREFIX         = 'victoriabank_mia_';
-        const MOD_VERSION        = '1.0.1';
+        const MOD_VERSION        = '1.0.2';
 
         const SUPPORTED_CURRENCIES = array('MDL');
         const ORDER_TEMPLATE       = 'Order #%1$s';
