f73037531c309e15df3f1a5bebd56bb01a8c0532
Alexander Minza <alexander.minza@gmail.com>
2026-01-18 10:57:43 +0200

    apply_filters wp_strip_all_tags

--- payment-gateway-wc-victoriabank-mia.php
+++ payment-gateway-wc-victoriabank-mia.php
@@ -1013,7 +1013,7 @@ function victoriabank_mia_plugins_loaded_init()
         protected function get_order_description(\WC_Order $order)
         {
             $description = sprintf($this->order_template, $order->get_id());
-            return apply_filters('victoriabank_mia_order_description', $description, $order);
+            return wp_strip_all_tags(apply_filters('victoriabank_mia_order_description', $description, $order));
         }
 
         protected function get_test_message(string $message)
@@ -1029,14 +1029,14 @@ function victoriabank_mia_plugins_loaded_init()
         protected function get_redirect_url(\WC_Order $order)
         {
             $redirect_url = $this->get_return_url($order);
-            return apply_filters('victoriabank_mia_redirect_url', $redirect_url, $order);
+            return (string) apply_filters('victoriabank_mia_redirect_url', $redirect_url, $order);
         }
 
         protected function get_callback_url()
         {
             // https://developer.woocommerce.com/docs/extensions/core-concepts/woocommerce-plugin-api-callback/
             $callback_url = WC()->api_request_url("wc_{$this->id}");
-            return apply_filters('victoriabank_mia_callback_url', $callback_url);
+            return (string) apply_filters('victoriabank_mia_callback_url', $callback_url);
         }
 
         protected static function get_logs_url()
