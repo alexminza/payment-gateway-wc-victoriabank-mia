cf570e0b75e59838ea40037d50db88977382b28b
Alexander Minza <alexander.minza@gmail.com>
2026-04-30 15:08:20 +0300

    languages

--- payment-gateway-wc-victoriabank-mia.php
+++ payment-gateway-wc-victoriabank-mia.php
@@ -17,7 +17,7 @@
  * Requires at least: 4.8
  * Tested up to: 6.9
  * WC requires at least: 3.3
- * WC tested up to: 10.5.2
+ * WC tested up to: 10.7.0
  * Requires Plugins: woocommerce
  *
  * @package payment-gateway-wc-victoriabank-mia
